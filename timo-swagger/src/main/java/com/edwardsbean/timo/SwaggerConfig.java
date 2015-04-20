0435c95da555d4a5a5cf8b806a4ba7450aa8fa2a
edwardsbean@gmail.com <edwardsbean@gmail.com>
2015-04-20 17:56:16 +0800

    add swagger

---
+++ timo-swagger/src/main/java/com/edwardsbean/timo/SwaggerConfig.java
@@ -0,0 +1,54 @@
+package com.edwardsbean.timo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
+import com.mangofactory.swagger.models.dto.ApiInfo;
+import com.mangofactory.swagger.plugin.EnableSwagger;
+import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
+/**
+ * 用于加载swagger相关类
+ * Created by edwardsbean on 2015/4/7.
+ */
+@Configuration
+@EnableSwagger
+public class SwaggerConfig
+{
+
+    private SpringSwaggerConfig springSwaggerConfig;
+
+    /**
+     * Required to autowire SpringSwaggerConfig
+     */
+    @Autowired
+    public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig)
+    {
+        this.springSwaggerConfig = springSwaggerConfig;
+    }
+
+    /**
+     * Every SwaggerSpringMvcPlugin bean is picked up by the swagger-mvc
+     * framework - allowing for multiple swagger groups i.e. same code base
+     * multiple swagger resource listings.
+     */
+    @Bean
+    public SwaggerSpringMvcPlugin customImplementation()
+    {
+        return new SwaggerSpringMvcPlugin(this.springSwaggerConfig).apiInfo(apiInfo()).includePatterns(
+                ".*?");
+    }
+
+    private ApiInfo apiInfo()
+    {
+        ApiInfo apiInfo = new ApiInfo(
+                "工程在线文档标题",
+                "工程在线文档描述",
+                "My Apps API terms of service",
+                "邮箱",
+                "My Apps API Licence Type",
+                "My Apps API License URL");
+        return apiInfo;
+    }
+}
