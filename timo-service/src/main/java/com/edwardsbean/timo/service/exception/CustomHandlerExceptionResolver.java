b1075796b4c3448601d5adaa70d34f6663ea1728
edwardsbean <edwardsbean@gmail.com>
2015-05-06 18:21:36 +0800

    format exception code

--- timo-service/src/main/java/com/edwardsbean/timo/service/exception/CustomHandlerExceptionResolver.java
+++ timo-service/src/main/java/com/edwardsbean/timo/service/exception/CustomHandlerExceptionResolver.java
@@ -12,37 +12,49 @@ import java.io.PrintWriter;
 
 /**
  * 控制器全局异常处理
- * Created by edwardsbean on 14-10-29.
+ * @author edwardsbean
+ * @date 14-10-29.
  */
 public class CustomHandlerExceptionResolver extends DefaultHandlerExceptionResolver {
     @Override
     public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
         if (!(request.getHeader("accept").indexOf("application/json") > -1 || (request.getHeader("X-Requested-With") != null && request.getHeader("X-Requested-With").indexOf("XMLHttpRequest") > -1) || request
                 .getParameter("callback") != null)) {
-            //同步请求
+            //非Ajax请求
             //TODO:添加异常处理
-            super.resolveException(request, response, handler, ex);
+//            super.resolveException(request, response, handler, ex);
+            writeJsonResult(response, ex, false, request);
+
         } else {
             // 异步请求
-            try {
-                PrintWriter writer = response.getWriter();
-                String callback = request.getParameter("callback");
-                ObjectMapper mapper = new ObjectMapper();
-                Msg msgCode = new Msg();
-                msgCode.setCode("1");
-                msgCode.setMsg(ex.getLocalizedMessage());
-                String json = mapper.writeValueAsString(msgCode);
-                if (callback != null) {
-                    callback = callback + "(" + json + ")";
-                    writer.write(callback);
-                } else {
-                    writer.write(json);
-                }
-                writer.flush();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            writeJsonResult(response, ex, true, request);
         }
         return null;
+    }
+
+    private void writeJsonResult(HttpServletResponse response, Exception ex, boolean isAjax, HttpServletRequest request) {
+        PrintWriter writer = null;
+        try {
+            writer = response.getWriter();
+            ObjectMapper mapper = new ObjectMapper();
+            Msg msgCode = new Msg();
+            msgCode.setCode("1");
+            msgCode.setMsg(ex.getLocalizedMessage());
+            String callback = request.getParameter("callback");
+            String json = mapper.writeValueAsString(msgCode);
+            if (callback != null) {
+                callback = callback + "(" + json + ")";
+                writer.write(callback);
+            } else {
+                writer.write(json);
+            }
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (writer != null) {
+                writer.close();
+            }
+        }
     }
 }
